4541ce60cb416716397dd7850f73e45c45c9ddca
Michael Leow <leow@sinarproject.org>
2017-04-12 04:02:36 +0800

    Summary: Tested stephanos/clock for mocking time
    Changesets:
    - Gets a negative time easily done
    - Tested normal implementation

--- GOLANG_KL/TIMERS/main.go
+++ GOLANG_KL/TIMERS/main.go
@@ -4,10 +4,25 @@ import (
 	"fmt"
 	// "time"
 	"github.com/jonboulle/clockwork"
+	"github.com/stephanos/clock"
+	"time"
 )
 
 func main() {
-	doLongTask(5, clockwork.NewRealClock())
+	// 	doLongTask(5, clockwork.NewRealClock())
+	sClock(5, clock.New())
+}
+
+func sClock(myduration int, c clock.Clock) {
+	currentTime := c.Now()
+	fmt.Println("Started at: ", currentTime)
+	c.Sleep(time.Duration(myduration) * time.Second)
+	for i := 0; i < 10000000; i++ {
+		// noop
+		// fmt.Print(i)
+	}
+	fmt.Println("==========================")
+	fmt.Println("Now ended : ", c.Now().Sub(currentTime).Seconds())
 }
 
 func doLongTask(myduration int, c clockwork.Clock) {
--- GOLANG_KL/TIMERS/main_test.go
+++ GOLANG_KL/TIMERS/main_test.go
@@ -7,7 +7,22 @@ import (
 	"github.com/jonboulle/clockwork"
 	"github.com/gavv/monotime"
 	"fmt"
+	"github.com/stephanos/clock"
 )
+
+func TestSClock(t *testing.T) {
+	currentMonoTime := monotime.Now()
+
+	c := clock.NewMock()
+	start := c.Now()
+	sClock(5, c)
+	c.Add(-30 * time.Second)
+	fmt.Println("End Time: ", c.Now().Sub(start).Seconds())
+
+	fmt.Println("==========================")
+	fmt.Println("Elpased monotime: ", monotime.Since(currentMonoTime).Seconds())
+
+}
 
 func TestSomething(t *testing.T) {
 	c := clockwork.NewFakeClockAt(time.Now())
@@ -19,14 +34,8 @@ func TestSomething(t *testing.T) {
 		doLongTask(1, c)
 		fmt.Println("==========================")
 		fmt.Println("Elpased monotime: ", monotime.Since(currentMonoTime).Seconds())
-
 		wg.Done()
 	}()
-
-	// c.BlockUntil(1)
-
-	// Go forward in time 1 minute
-	c.Advance(30 * time.Second)
 
 	wg.Wait()
 }
