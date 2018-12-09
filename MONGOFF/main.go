0e9b4b5e9b58e111a69fb4d7347379e318f62855
Michael Leow <leow@sinarproject.org>
2018-12-09 21:37:02 +0800

    Summary: Collect info for broken please BUILD
    Changesets:
    - Basic mongodb connection; cluster is down :(
    - FIgured out the ImportPaths for the use of own lib
    - Started moving some libs to third_party structure

--- MONGOFF/main.go
+++ MONGOFF/main.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"fmt"
-	"repo"
+	"log"
+	"time"
+
+	"github.com/globalsign/mgo"
+	"github.com/leowmjw/playground-golang/MONGOFF/repo"
+)
+
+const (
+	MongoDBHosts = ""
+	AuthDatabase = ""
+	AuthUserName = "mleow"
+	AuthPassword = ""
+	TestDatabase = ""
 )
 
 func main() {
 
 	fmt.Println("MongoDB + FF! Cool!!")
 
-	// repo.New()
-	// repo.ReadSecrets()
+	// We need this object to establish a session to our MongoDB.
+	mongoDBDialInfo := &mgo.DialInfo{
+		Addrs:    []string{MongoDBHosts},
+		Timeout:  1 * time.Second,
+		Database: AuthDatabase,
+		Username: AuthUserName,
+		Password: AuthPassword,
+	}
 
+	// Create a session which maintains a pool of socket connections
+	// to our MongoDB.
+	mongoSession, err := mgo.DialWithInfo(mongoDBDialInfo)
+	if err != nil {
+		log.Fatalf("CreateSession: %s\n", err)
+	} else {
+		log.Println("All OK!!")
+	}
+
+	defer mongoSession.Close()
+	// // Init; with real/dummy
 	repo.New()
+	// // Gte the needed data ..
+	// repo.ReadSecrets()
 }
--- MONGOFF/repo/repo.go
+++ MONGOFF/repo/repo.go
@@ -2,14 +2,35 @@ package repo
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/globalsign/mgo"
 )
+
+var mongoDBURL string
+var mongoSession *mgo.Session
 
 // New will create a new Repo instance tied with MongoDB implementation
 func New() {
 	fmt.Println("Initializing repo with the MongoDB instance ..")
+	mongoDBURL = os.Getenv("MONGODB_URL")
+	if mongoDBURL == "" {
+		panic("MONGODB_URL needs to be defined!! e.g mongodb://<dbuser>:<dbpassword>@ds121183.mlab.com:21183/cooljoe")
+	}
+	session, err := mgo.Dial(mongoDBURL)
+	if err != nil {
+		panic(err)
+	}
+	mongoSession = session
 }
 
 // ReadSecrets get data out from the Vautl server ..
 func ReadSecrets() {
 	fmt.Println("Reading .. secretes ..")
+	c := mongoSession.DB("cooljoe").C("quotes")
+	q, err := c.Count()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("COUNT", q)
 }
