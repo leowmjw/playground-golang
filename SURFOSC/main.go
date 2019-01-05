7a33b4df4ab5b52c0ecc4b9b602bd8e553ab97bf
Michael Leow <leow@sinarproject.org>
2019-01-05 20:06:44 +0800

    Summary: Add basic Demo forSearch Form + result for top level OSCv3

--- SURFOSC/main.go
+++ SURFOSC/main.go
@@ -5,10 +5,54 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/y0ssar1an/q"
 	"gopkg.in/headzoo/surf.v1"
+	"gopkg.in/headzoo/surf.v1/agent"
 )
 
 func main() {
 	fmt.Println("Welcome to gomod SurfOSCv3!!")
+	//BasicRedditDemo()
+	BasicOSCDemo()
+}
+
+func BasicOSCDemo() {
+
+	// Go to the OSCv3 page and fill up the form and post it!
+	bow := surf.NewBrowser()
+	bow.SetUserAgent(agent.Firefox())
+	err := bow.Open("http://www.epbt.gov.my/osc/Carian_Projek.cfm")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, form := range bow.Forms() {
+		if form != nil {
+			//spew.Dump(form.Method())
+			// See as HTML
+			q.Q(form.Dom().Html())
+			// See form as Text
+			q.Q(form.Dom().Text())
+			// Default it is search by Requester
+			// Search by project name (e.g Summary description)
+			form.Input("Pilih", "3")
+			// Seach term; leave empty for everything
+			form.Input("Cari", "PENGAWAL")
+			// Select Taman Perindustrian Kulim
+			form.Input("BahKod", "0212")
+			// Submit
+			if form.Submit() != nil {
+				panic(err)
+			}
+			// see the results as HTML
+			q.Q(bow.Dom().Html())
+			//see the results as Text
+			q.Q(bow.Dom().Text())
+			// See all the links
+			q.Q(bow.Links())
+		}
+	}
+}
+
+func BasicRedditDemo() {
 	// Create a new browser and open reddit.
 	bow := surf.NewBrowser()
 	err := bow.Open("http://reddit.com")
