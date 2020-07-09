4d5d728338d663248497dc1ea521e69fb684794f
Michael Leow <mleow@moneylion.com>
2020-07-09 23:43:40 +0800

    Summary: Fix the handler func must be unique

--- HTTPTEST_EXAMPLE/api/user.go
+++ HTTPTEST_EXAMPLE/api/user.go
@@ -7,8 +7,8 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
-// Handler for UserService ..
-func Handler(w http.ResponseWriter, r *http.Request) {
+// UserHandler for UserService ..
+func UserHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Inside User function!!!</h1>")
 	spew.Dump(r.Context().Value)
 }
